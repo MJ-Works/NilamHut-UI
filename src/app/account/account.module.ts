0f46bbe6eb051635c07fe847d6dc5f1874cef8de
jaggesher <jaggesher14@gmail.com>
2018-05-24 04:29:26 +0600

    signin Page design complete

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -4,10 +4,21 @@ import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { AccountRoutingModule } from './/account-routing.module';
 
+
+import {MatCardModule} from '@angular/material/card';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
 @NgModule({
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [SigninComponent, SignupComponent]
 })
