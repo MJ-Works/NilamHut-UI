cb5338904d70e9997f3f945149d03991b1cf641b
jaggesher <jaggesher14@gmail.com>
2018-05-24 15:32:21 +0600

    new account complete

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  public signupForm: FormGroup;
+  public name: FormControl;
+  public email: FormControl;
+  public password: FormControl;
+  public hide: boolean = true;
+  
+  private CreateForm(): void {
+    this.signupForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      password: this.password
+    });
+  }
+
+  private CreateFormControls(): void {
+    this.name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(100)
+    ]);
+    this.email = new FormControl('',[
+      Validators.required,
+      Validators.email
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(100)
+    ]);
+  }
+  
   constructor() { }
 
   ngOnInit() {
+    this.CreateFormControls();
+    this.CreateForm();
+  }
+
+  onSubmit() {
+    console.log("Ok Boss");
+    console.log(this.signupForm.value);
   }
 
 }
