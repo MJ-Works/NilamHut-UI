626af4a6b53ba6f59700bb1ff637b9488e4cf101
IAmMonmoy <iammonmoy@gmail.com>
2018-05-26 16:55:10 +0600

    added basic files for product add

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './/product-routing.module';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
-  declarations: [ViewProductComponent]
+  declarations: [ViewProductComponent, AddProductComponent]
 })
 export class ProductModule { }
