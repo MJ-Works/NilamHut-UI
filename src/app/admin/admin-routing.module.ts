6802067510de2ea3e02935bc772bcb36e5362318
jaggesher <jaggesher14@gmail.com>
2018-05-28 17:32:38 +0600

    location and category component added

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationComponent } from './location/location.component';
+import { CategoryComponent } from './category/category.component';
 
 const routes: Routes = [
-
+  { path: "location", component: LocationComponent },
+  { path: "category", component: CategoryComponent }
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './/admin-routing.module';
+import { LocationComponent } from './location/location.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: []
+  declarations: [LocationComponent, CategoryComponent]
 })
 export class AdminModule { }
