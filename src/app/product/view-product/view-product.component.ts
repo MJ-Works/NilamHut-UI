8ec432c4eecdb3ee3f14b5ef37fb43a591ba0ac0
IAmMonmoy <iammonmoy@gmail.com>
2018-05-28 12:15:40 +0600

    on click image show complete

--- src/app/product/view-product/view-product.component.ts
+++ src/app/product/view-product/view-product.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-product.component.css']
 })
 export class ViewProductComponent implements OnInit {
-
-  constructor() { }
+  public bigPicture = "assets/image/14358915_10207878050809041_5592658587766789579_n.jpg";
+  constructor() { 
+  }
 
   ngOnInit() {
+    
+  }
+  onImageClick($event)
+  {
+    this.bigPicture = $event.srcElement.attributes.src.value;
   }
 
 }
